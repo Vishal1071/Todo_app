9ff0e182ae1f6134f622c4a7a4dacd10ff191d17
vishal sonagara <vishalsonagara48@gmail.com>
2026-06-04 18:29:43 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Todoapp from './todo/todoapp.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Todoapp />
+    </>
+  )
+}
+
+export default App
---
+++ src/todo/todoapp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+import { useState } from 'react'
+
+const Todoapp = () => {
+    
+  return (
+    <>
+      <h1>Todo app</h1>
+    </>
+  )
+}
+
+export default Todoapp
